7b686cf560dd24e2db69d566ac9f3fab91facc67
James Niyongira <lascasasjames85@gmail.com>
2023-09-20 11:34:50 -0400

    bug tracing

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const express = require('express');
-//
-// const app = express();
-//
-// app.get('/', (req, res) => {
-//     res.status(200).json({message:'Hi there 2 23 test again'});
-// });
-//
-//
-// app.listen(8080, () => {
-//     console.log('Listening on port 8080');
-// });
+const express = require('express');
 
-console.log("hello world")
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:'Hi there, this should definitely work'});
+});
+
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
+
+// console.log("hello world")
