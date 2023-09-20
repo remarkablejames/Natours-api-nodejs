b954846a7669d577dbc8d62748b80fc0f4a4070d
James Niyongira <lascasasjames85@gmail.com>
2023-09-20 12:01:47 -0400

    scaffolding

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const morgan  = require('morgan')
 
 const app = express();
 
+// MIDDLEWARES
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log("hello from the middleware!")
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.status(200).json({message:'Hi there, this should definitely work'});
+    res.status(200).json({message:"Hello world"});
+
 });
 
 
